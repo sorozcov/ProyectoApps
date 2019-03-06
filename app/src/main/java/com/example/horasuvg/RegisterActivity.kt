47d527f1244a15f6879255efaf3a6f62d69f7a52
DouglasDL28 <dele18037@uvg.edu.gt>
2019-03-06 07:43:54 -0600

    Agregué RegisterActivity.

---
+++ app/src/main/java/com/example/horasuvg/RegisterActivity.kt
@@ -0,0 +1,21 @@
+package com.example.horasuvg
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class RegisterActivity : AppCompatActivity() {
+
+//    fun adminRegisterButton (view: View) {
+//        val intent = Intent(this@MainActivity, CreateContactActivity::class.java)
+//        startActivity(intent)
+//    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+
+    }
+}
